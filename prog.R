400b6ec2b21353f4070e5f6584c38d6732a64c41
shivani2695 <66261934+shivani2695@users.noreply.github.com>
2020-06-01 20:27:14 +0530

    Add files via upload

--- prog.R
+++ prog.R
@@ -1,1 +1,15 @@
 #Write your code here
+install.packages("tidyverse")
+library(readr)
+library(dplyr)
+fda <- read_csv("fda.csv")
+str(fda)
+
+after2005 <- fda %>%
+	filter(issued >= "2005-01-01") %>%
+	arrange(issued)
+
+count_letters <- fda %>%
+	mutate(year = format(issued, "%Y")) %>%
+	group_by(year) %>%
+	summarize(letters=n())
